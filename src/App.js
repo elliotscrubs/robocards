f19270d01b3ceeeff48070ba0e219026a9166f24
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-09-10 19:14:56 +0200

    robo cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  const fetchUserData = () => {
+    fetch('https://random-data-api.com/api/users/random_user?size=12')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {users.map(user => (
+        <div key={user.id} className='flip-card'>
+          <div className='flip-card-inner'>
+            <div className='flip-card-front'>
+              <h2>
+                {user.first_name} {user.last_name}
+              </h2>
+              <h4>{user.employment.title}</h4>
+              <img src={user.avatar} alt='avatar' />
+            </div>
+            <div className='flip-card-back'>
+              <h1>More information here</h1>
+              <h2>{user.email}</h2>
+              <h2>{user.phone_number}</h2>
+              <h2>{user.date_of_birth}</h2>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }

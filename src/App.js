bfa3d3a3c34db0cff62167d38cd42803ee141ddf
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-09-11 10:33:07 +0200

    fetch button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   console.log(users);
 
   const fetchUserData = () => {
-    fetch('https://random-data-api.com/api/users/random_user?size=12')
+    fetch('https://random-data-api.com/api/users/random_user?size=8')
       .then(response => {
         return response.json();
       })
@@ -20,26 +20,31 @@ function App() {
   }, []);
 
   return (
-    <div className='container'>
-      {users.map(user => (
-        <div key={user.id} className='flip-card'>
-          <div className='flip-card-inner'>
-            <div className='flip-card-front'>
-              <h2>
-                {user.first_name} {user.last_name}
-              </h2>
-              <h4>{user.employment.title}</h4>
-              <img src={user.avatar} alt='avatar' />
-            </div>
-            <div className='flip-card-back'>
-              <h1>More information here</h1>
-              <h2>{user.email}</h2>
-              <h2>{user.phone_number}</h2>
-              <h2>{user.date_of_birth}</h2>
+    <div>
+      <button className='button' onClick={() => window.location.reload()}>
+        Fetch now
+      </button>
+      <div className='container'>
+        {users.map(user => (
+          <div key={user.id} className='flip-card'>
+            <div className='flip-card-inner'>
+              <div className='flip-card-front'>
+                <h2>
+                  {user.first_name} {user.last_name}
+                </h2>
+                <h4>{user.employment.title}</h4>
+                <img src={user.avatar} alt='avatar' />
+              </div>
+              <div className='flip-card-back'>
+                <h1>More information here</h1>
+                <h2>{user.email}</h2>
+                <h2>{user.phone_number}</h2>
+                <h2>{user.date_of_birth}</h2>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
